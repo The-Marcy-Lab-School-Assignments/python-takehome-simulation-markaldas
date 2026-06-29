0b084369372c7dea5ca66fb2fb8630dcfe1e3baa
markaldas <markaldas41@gmail.com>
2026-06-29 00:24:57 -0400

    finished part 2

--- README.md
+++ README.md
@@ -15,6 +15,9 @@ Output will be saved to `output.txt`. The console will confirm when the file has
 Part 1:
 My script finds the total number of open request in the csv file by adding to a variable whenever it encounters the key of open_request's value of open. It then also adds all the complaint types in the csv file and puts them in a dict with the value of how many times that complaint shows up in the csv file. I then find the complaint with the highest value and display that. I then added the boroughs to a dict counted how many times the borough shows up in the requests.
 
+Part 2:
+My script finds all the complaint types and counts how many there are per complaint type. It also looks for the boro with most open request by looking for boroughs with open requests, counting them for each borough and then getting the one with the max amount. My script also looks for all the boroughs that have closed cases and divides that by the amount of total cases, then it displays each borough and the percentage of closure. The script lastly displays the top 3 boroughs with most requests and it does this by checking each borough counting the requests and then sorting the boroughs by how many request they have and then taking only the top 3.
+
 ## Dependencies
 
 This script uses only Python's built-in libraries: `csv`.
--- analysis.py
+++ analysis.py
@@ -13,37 +13,64 @@ def data(r):
     open_request = 0
     type_complaint = {}
     request_per_bor = {}
+    boro_w_most_open_req = {}
     for row in r:
         if 'complaint_type' in row:
             type_complaint[row['complaint_type']] = type_complaint.get(row['complaint_type'], 0) + 1
         if 'borough' in row: 
             request_per_bor[row['borough']] = request_per_bor.get(row['borough'], 0) + 1
+        if 'borough' in row and row['resolution_status'] == "Open":
+            boro_w_most_open_req[row['borough']] = boro_w_most_open_req.get(row['borough'], 0) + 1
         if row['resolution_status'] == "Open":
             open_request += 1
+
     
     find_max_complaint = max(type_complaint, key=type_complaint.get)
+    find_max_open_boro = max(boro_w_most_open_req, key=boro_w_most_open_req.get)
 
     sorted_boro = sorted(request_per_bor.items())
     
     boro = ""
+    request_per_complaint = ""
+    closure_rate = ""
+    top_3_boro_req = ""
+
     for key, value in sorted_boro:
-        boro += f"-{key}: {value}\n"
+        boro += f"- {key}: {value}\n"
 
-    print(boro)
+    for key, value in type_complaint.items():
+        request_per_complaint += f"- {key}: {value}\n"
 
-    return open_request, {find_max_complaint, type_complaint[find_max_complaint]}, boro
+    for key, value in sorted(request_per_bor.items()):
+        closed = value - boro_w_most_open_req[key]
+        rate = round((closed / value) * 100, 1)
+        closure_rate += f"- {key}: {rate}%\n"
 
-open_rq, t_complaint, boro = data(rows)
+    sorted_boroughs = sorted(request_per_bor.items(), key=lambda item: (-item[1], item[0]))[:3]
+
+    count = 0
+    for key, value in sorted_boroughs:
+        count += 1
+        top_3_boro_req += f"{count}. {key} ({value} requests)\n"
+
+    print(top_3_boro_req)
+    return open_request, {find_max_complaint, type_complaint[find_max_complaint]}, boro, request_per_complaint, {find_max_open_boro, boro_w_most_open_req[find_max_open_boro]}, closure_rate, top_3_boro_req
 
 
-    
+open_rq, t_complaint, boro, rq_per_complaint, boro_open_req, boro_req_rate, top_3_boro_req = data(rows)
 
-meaning_of_life = 42
+
+
 
 with open('output.txt', 'w') as f:
     t_complaint_value, t_complaint_key = t_complaint
     f.write(f"""Open Requests: {open_rq} 
     \nMost common complaint type: {t_complaint_value} ({t_complaint_key} requests)
     \nRequests per borough: \n{boro} """) 
+
+    boro_req_num, boro_name = boro_open_req
+    f.write(f"""\nRequests by complaint type: \n{rq_per_complaint} \nBorough with most open requests: {boro_name} ({boro_req_num} open)
+            \nClosure rate by borough: \n{boro_req_rate}\nTop 3 boroughs by total requests:\n{top_3_boro_req}""")
+
 
 print("Output saved to output.txt")
